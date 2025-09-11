940b3360dfcd4720055139ce30106ac1a1a360e8
sanesdotio <sander.neset.dev@gmail.com>
2025-09-11 16:30:08 +0200

    test ci

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,8 @@ import express from 'express';
 const app = express();
 const port = 8080;
 
+console.log('We are testing CI pipeline');
+
 app.listen(port, () => {
 	console.log(`Server is running on port ${port}`);
 });
